e85bde11efae40fc652188fc16c248efa4f5bc60
Iris Valle <iris.valle.design@gmail.com>
2021-03-24 23:40:52 -0400

    Updated README and js code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # work-day-scheduler
+
+This is a calendar application that allows a user to save events for each hour of the day and features dynamically updated HTML and CSS powered by jQuery. 
+
+Here is the link to the deployed application: [scheduler] (https://icvalle.github.io/work-day-scheduler/)
+
+The following image shows the web application’s appearance.
+![scheduler screenshot](assets/images/scheduler_screenshot.png)
--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,6 +1,6 @@
 //Current Date needs to be generated & displayed upon rendering of the page
 $(document).ready(function() {
-
+    
     var today = moment();
     $('#currentDay').text(today.format("dddd, MMMM Do"));
 
@@ -23,14 +23,71 @@ $(document).ready(function() {
     }
 
 //When Save Button is clicked then text is saved in local storage and persists even when pg is refreshed
+    
+    $(timeBlockEl[8]).children().eq(2).on('click', function () {
+        var h17Event = $(timeBlockEl[8]).children().eq(1).val();
+        localStorage.setItem("event5PM", h17Event);
+        var event5PMSaved = localStorage.getItem("event5PM")
+        var hr17El = $(timeBlockEl[8]).children().eq(1);
+        hr17El.text(event5PMSaved);
+    });
 
-    $('.saveBtn').on('click', function() {
-        var eventInput = $(this).siblings('textarea').val();
-
-        localStorage.setItem('schedule-' + i, eventInput);
-
+    $(timeBlockEl[7]).children().eq(2).on('click', function () {
+        var h16Event = $(timeBlockEl[7]).children().eq(1).val();
+        localStorage.setItem("event4PM", h16Event);
+        localStorage.getItem("event4PM");
         
     });
+
+    $(timeBlockEl[6]).children().eq(2).on('click', function () {
+        var h15Event = $(timeBlockEl[6]).children().eq(1).val();
+        localStorage.setItem("event3PM", h15Event);
+        localStorage.getItem("event3PM");
+        
+    });
+
+    $(timeBlockEl[5]).children().eq(2).on('click', function () {
+        var h14Event = $(timeBlockEl[5]).children().eq(1).val();
+        localStorage.setItem("event2PM", h14Event);
+        localStorage.getItem("event2PM");
+        
+    });
+
+    $(timeBlockEl[4]).children().eq(2).on('click', function () {
+        var h13Event = $(timeBlockEl[4]).children().eq(1).val();
+        localStorage.setItem("event1PM", h13Event);
+        localStorage.getItem("event1PM");
+        
+    });
+
+    $(timeBlockEl[3]).children().eq(2).on('click', function () {
+        var h12Event = $(timeBlockEl[3]).children().eq(1).val();
+        localStorage.setItem("event12PM", h12Event);
+        localStorage.getItem("event12PM");
+        
+    });
+
+    $(timeBlockEl[2]).children().eq(2).on('click', function () {
+        var h11Event = $(timeBlockEl[2]).children().eq(1).val();
+        localStorage.setItem("event11AM", h11Event);
+        localStorage.getItem("event11AM");
+        
+    });
+
+    $(timeBlockEl[1]).children().eq(2).on('click', function () {
+        var h10Event = $(timeBlockEl[1]).children().eq(1).val();
+        localStorage.setItem("event10AM", h10Event);
+        localStorage.getItem("event10AM");
+        
+    });
+
+    $(timeBlockEl[0]).children().eq(2).on('click', function () {
+        var h9Event = $(timeBlockEl[0]).children().eq(1).val();
+        localStorage.setItem("event9AM", h9Event);
+        localStorage.getItem("event9AM");
+        
+    });
+
 
 
 
